6c94d8ff73f7a20a53df75d30b01c6dca9963364
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-04-11 15:17:36 +0530

    menu updated

--- src/components/common/NavbarMobile.jsx
+++ src/components/common/NavbarMobile.jsx
@@ -1,121 +1,109 @@
-import { IoMdMenu } from "react-icons/io";
+import { LuMenu } from "react-icons/lu";
 import { IoMdClose } from "react-icons/io";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import { useDispatch, useSelector } from "react-redux";
 import gsap from "gsap";
 import { useRouter } from "next/router";
 import Modal from "./Modal";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import { VscAccount } from "react-icons/vsc";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
 gsap.registerPlugin(ScrollTrigger);
-const NavbarMobile = ({ openBag, setOpenBag , headerNav}) => {
 
-    const [menu, setMenu] = useState([]);
+const NavbarMobile = ({ openBag, setOpenBag, headerNav }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const dispatch = useDispatch();
     const router = useRouter();
     const path = router.pathname;
     const pathName = router.pathname.split("/")[1];
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [color, setColor] = useState(null);
-    const [update, setUpdate] = useState(false);
     const user = useSelector((state) => state.user.user);
     const cartCount = useSelector((state) => state.cart.itemcount);
-  
-    function openModal() {
-      setModalIsOpen(true);
-    }
-  
-    function closeModal() {
-      setModalIsOpen(false);
-      setOpenBag(false);
-    }
+    const menuTL = useRef(null);
+
+    const openModal = () => setModalIsOpen(true);
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+        setOpenBag(false);
+    };
 
     useGSAP(() => {
-        const sideNavbar = document.getElementById('side-navbar')
-        const closeMenuBtn = document.getElementById('closeMenu-btn')
-        const menuBtn = document.getElementById('menu-btn')
-        const sideMenuLinks = document.querySelectorAll('.side-menu-links a')
-        const bag = document.querySelector('#bag')
-        const logo = document.querySelector('.menulogo')
+        menuTL.current = gsap.timeline({ paused: true })
+            .to(".line1m", {
+                top: "50%",
+                transform: "translateY(-50%)",
+                duration: 0.2
+            }, "a")
+            .to(".line2m", {
+                top: "50%",
+                transform: "translateY(-50%)",
+                duration: 0.2
+            }, "a")
+            .to(".line1m", {
+                rotate: 45,
+                duration: 0.2
+            }, "b")
+            .to(".line2m", {
+                rotate: -45,
+                duration: 0.2
+            }, "b")
+            .to("#side-navbar", {
+                clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+                duration: 0.3,
+                ease: "power2.out",
+            }, "a")
+            .to(".side-menu-links",{
+                y:50,
+                duration:0.3
+            },"a")
 
-        menuBtn.addEventListener('click', () => {
-            closeModal()
-            gsap.to(sideNavbar,{
-                left: 0,
-                duration: 0.2,
-                ease: 'power2.out'
-            })
-        })
+    }, []);
 
-        closeMenuBtn.addEventListener('click', () => {
-            gsap.to(sideNavbar,{
-                left: '100%',
-                duration: 0.2,
-                ease: 'power2.out'
-            })
-        })
-
-        sideMenuLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                gsap.to(sideNavbar,{
-                    left: '100%',
-                    duration: 0.2,
-                    ease: 'power2.out'
-                })
-            })
-        })
-        bag.addEventListener('click', () => {
-            gsap.to(sideNavbar,{
-                delay: 1,
-                left: '100%',
-                duration: 0.2,
-                ease: 'power2.out'
-            })
-        })
-        logo.addEventListener('click', () => {
-            gsap.to(sideNavbar,{
-                left: '100%',
-                duration: 0.2,
-                ease: 'power2.out'
-            })
-        })
-
-    }, [])
+    const toggleMenu = () => {
+        setIsMenuOpen(prev => {
+            const newState = !prev;
+            if (newState) {
+                menuTL.current.play();
+            } else {
+                menuTL.current.reverse();
+            }
+            return newState;
+        });
+    };
 
     return (
         <>
-        <div className='navbar-mobile'>
-            <Link href="/" id='nav-logo'>de ve she dreams</Link>
-            <span id='menu-btn'>
-                <IoMdMenu size={30} />
-            </span>
-            <div id='side-navbar'>
-                <div className='side-menu-logo'>
-                    <Link href="/" id='nav-logo' className="menulogo">de ve she dreams</Link>
-                    <span id='closeMenu-btn'>
-                        <IoMdClose size={30} />
-                    </span>
+            <div className='navbar-mobile'>
+                <Link href="/" id='nav-logo'>de ve she dreams</Link>
+                <div className="menu-icons">
+                    <Link href="/profile"><VscAccount size={25}/></Link>
+                    <HiOutlineShoppingBag onClick={openModal} size={25} />
+                    <div id='menu-btn' onClick={toggleMenu}>
+                        <span className="line1m linem"></span>
+                        <span className="line2m linem"></span>
+                    </div>
                 </div>
-                <div className='side-menu-links'>
-                    <Link href="/shop">shop</Link>
-                    <Link href="/archives">archives</Link>
-                    <Link href="/about">about</Link>
-                    <Link href="/contact">contact</Link>
-                    <Link id='about-link' href="/profile">account</Link>
-                    <span id="bag" onClick={openModal}>bag</span>
+                <div id='side-navbar'>
+                    <div className='side-menu-links'>
+                        <Link href="/shop">shop</Link>
+                        <Link href="/archives">archives</Link>
+                        <Link href="/about">about</Link>
+                        <Link href="/contact">contact</Link>
+                    </div>
                 </div>
             </div>
-        </div>
-        <Modal
-          closeModal={closeModal}
-          modalIsOpen={modalIsOpen}
-          setOpenBag={setOpenBag}
-          openBag={openBag}
-          setModalIsOpen={setModalIsOpen}
-        />
+            <Modal
+                closeModal={closeModal}
+                modalIsOpen={modalIsOpen}
+                setOpenBag={setOpenBag}
+                openBag={openBag}
+                setModalIsOpen={setModalIsOpen}
+            />
         </>
-    )
-}
+    );
+};
 
-export default NavbarMobile
+export default NavbarMobile;
