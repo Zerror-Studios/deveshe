d1d9fa900b1eb52e9ddb43c66d895130ebbe8e23
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-04-11 16:49:36 +0530

    pdp sticky in mobile

--- src/components/shop/ProductListing.jsx
+++ src/components/shop/ProductListing.jsx
@@ -2,91 +2,118 @@ import React, { useEffect, useState } from 'react'
 import styles from './shop.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
-
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
+gsap.registerPlugin(ScrollTrigger)
 const ProductListing = () => {
   const products = [
     {
-      image1:"/product/Emerald Kaftan top (2).jpeg",
-      image2:""
+      image1: "/product/Emerald Kaftan top (2).jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Gold crochet bib (2).jpeg",
-      image2:""
+      image1: "/product/Gold crochet bib (2).jpeg",
+      image2: ""
     },
-    
+
     {
-      image1:"/product/Ombré blue magia sleeve dress (1).jpeg",
-      image2:""
-    },
-    {
-      image1:"/product/Ombré crochet patch dress- Brown (1).JPG",
-      image2:""
+      image1: "/product/Ombré blue magia sleeve dress (1).jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Emerald Kaftan top (2).jpeg",
-      image2:""
+      image1: "/product/Ombré crochet patch dress- Brown (1).JPG",
+      image2: ""
     },
     {
-      image1:"/product/Golden fishnet crochet bag.jpeg",
-      image2:""
+      image1: "/product/Emerald Kaftan top (2).jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Emerald Kaftan top (2).jpeg",
-      image2:""
+      image1: "/product/Golden fishnet crochet bag.jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Gold crochet bib (2).jpeg",
-      image2:""
-    },
-    
-    {
-      image1:"/product/Ombré blue magia sleeve dress (1).jpeg",
-      image2:""
+      image1: "/product/Emerald Kaftan top (2).jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Ombré crochet patch dress- Brown (1).JPG",
-      image2:""
+      image1: "/product/Gold crochet bib (2).jpeg",
+      image2: ""
+    },
+
+    {
+      image1: "/product/Ombré blue magia sleeve dress (1).jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Emerald Kaftan top (2).jpeg",
-      image2:""
+      image1: "/product/Ombré crochet patch dress- Brown (1).JPG",
+      image2: ""
     },
     {
-      image1:"/product/Golden fishnet crochet bag.jpeg",
-      image2:""
+      image1: "/product/Emerald Kaftan top (2).jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Emerald Kaftan top (2).jpeg",
-      image2:""
+      image1: "/product/Golden fishnet crochet bag.jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Gold crochet bib (2).jpeg",
-      image2:""
-    },
-    
-    {
-      image1:"/product/Ombré blue magia sleeve dress (1).jpeg",
-      image2:""
+      image1: "/product/Emerald Kaftan top (2).jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Ombré crochet patch dress- Brown (1).JPG",
-      image2:""
+      image1: "/product/Gold crochet bib (2).jpeg",
+      image2: ""
+    },
+
+    {
+      image1: "/product/Ombré blue magia sleeve dress (1).jpeg",
+      image2: ""
     },
     {
-      image1:"/product/Emerald Kaftan top (2).jpeg",
-      image2:""
+      image1: "/product/Ombré crochet patch dress- Brown (1).JPG",
+      image2: ""
     },
     {
-      image1:"/product/Golden fishnet crochet bag.jpeg",
-      image2:""
+      image1: "/product/Emerald Kaftan top (2).jpeg",
+      image2: ""
+    },
+    {
+      image1: "/product/Golden fishnet crochet bag.jpeg",
+      image2: ""
     },
   ]
   const [displayedProducts, setDisplayedProducts] = useState(products);
 
+  useGSAP(() => {
+    if(window.innerWidth > 576) return; 
+    const container = document.querySelector('#productCont');
+    const height = container.scrollHeight;
+    const containerHeight = document.querySelector("#productContwrap").getBoundingClientRect().height;
+    const scrollHeight = height - containerHeight*2;
+
+    gsap.to('#productCont', {
+      y: -840,
+      // duration: 0.5,
+      // ease: "power2.inOut",
+      scrollTrigger: {
+        trigger: '#productListing',
+        scroller: "body",
+        start: 'top top',
+        end: 'top -100%',
+        scrub: 1,
+        // markers: true,
+        pin: true
+      }
+    });
+
+  }, []);
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 576) {
-        setDisplayedProducts(products.slice(0, 6));
+        setDisplayedProducts(products.slice(0, 10));
       } else {
         setDisplayedProducts(products);
       }
@@ -98,24 +125,23 @@ const ProductListing = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
   return (
-    <div className={styles.productListing}>
+    <div className={styles.productListing} id='productListing'>
       <Link href="/product?id=6752e99c935fd014e82be779" className={styles.leftProCon}>
         <Image width={1000} height={1000} alt='image' src="/product/Ombré yellow wrap shirt.jpeg" />
         {/* <div className={styles.overlayLeftp}>
           <Image width={1000} height={1000} src="https://www.datocms-assets.com/136001/1727704761-vazzi-water-based-lubricant-100ml-bottle.png?auto=format&fit=crop&h=620&w=520" alt='image' />
         </div> */}
       </Link>
-      <div className={styles.rightProCon}>
-        {
-          displayedProducts.map((product, index) => (
-            <Link href={`/product?id=6752e99c935fd014e82be779`} key={index} className={styles.productCard}>
-              <Image width={1000} height={1000} alt='image' src={product.image1} />
-              {/* <div className={styles.overlayRightp}>
-            <Image width={1000} height={1000} src={product.image2} alt='image' />
-          </div> */}
-            </Link>
-          ))
-        }
+      <div className={styles.rightProCon} id='productContwrap'>
+        <div className={styles.rightProConWrap} id='productCont'>
+          {
+            displayedProducts.map((product, index) => (
+              <Link href={`/product?id=6752e99c935fd014e82be779`} key={index} className={styles.productCard}>
+                <Image width={1000} height={1000} alt='image' src={product.image1} />
+              </Link>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
