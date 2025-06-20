1703de6afa7db9f7f82b8630b0cbf257fdc49538
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-06-20 22:53:44 +0530

    updated path

--- src/pages/lookbook/index.jsx
+++ src/pages/lookbook/index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import styles from "@/components/lookbook/lookbook.module.css";
 import Section2 from "@/components/lookbook/Section2";
 import Section3 from "@/components/lookbook/Section3";
+import React from "react";
 
 const Lookbook = () => {
   return (
