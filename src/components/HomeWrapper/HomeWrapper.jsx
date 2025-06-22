9f7de7e0f991e7eac05d754c975bf9cbf9a30cdc
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-06-22 13:54:07 +0530

    responsive and plp changes

--- src/components/HomeWrapper/HomeWrapper.jsx
+++ src/components/HomeWrapper/HomeWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import Navbar from "../common/Navbar";
 import NavbarMobile from "../common/NavbarMobile";
@@ -7,11 +7,26 @@ import Navbar2 from "../common/Navbar2";
 
 const HomeWrapper = ({ children, openBag, setOpenBag }) => {
   const router = useRouter();
+  useEffect(() => {
+    const setVh = () => {
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    };
+    setVh();
+    window.addEventListener("resize", setVh);
+    return () => window.removeEventListener("resize", setVh);
+  }, []);
 
   return (
     <div className="home-wrapper">
-      {router.pathname !== "/" ? <Navbar2 openBag={openBag} setOpenBag={setOpenBag} /> : <Navbar openBag={openBag} setOpenBag={setOpenBag} />}
-      <NavbarMobile openBag={openBag} setOpenBag={setOpenBag}/>
+      {router.pathname !== "/" ? (
+        <Navbar2 openBag={openBag} setOpenBag={setOpenBag} />
+      ) : (
+        <Navbar openBag={openBag} setOpenBag={setOpenBag} />
+      )}
+      <NavbarMobile openBag={openBag} setOpenBag={setOpenBag} />
       {children}
       <Footer />
     </div>
--- src/components/shop/ProductListing.jsx
+++ src/components/shop/ProductListing.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./shop.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger);
 const ProductListing = () => {
   const products = [
@@ -42,66 +41,6 @@ const ProductListing = () => {
       image1: "/newproduct/BI07.jpg",
     },
   ];
-  const [displayedProducts, setDisplayedProducts] = useState(products);
-
-  useGSAP(() => {
-    if (window.innerWidth > 576) return;
-    const strip1Height =
-      document.querySelector("#productStrip1").getBoundingClientRect().height /
-      2;
-    const strip2Height = document
-      .querySelector("#productStrip2")
-      .getBoundingClientRect().height;
-    const productContHeight = document
-      .querySelector("#productCont")
-      .getBoundingClientRect().height;
-
-    const strip1Value = strip1Height - productContHeight;
-    const strip2Value = strip2Height - productContHeight;
-
-    var tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#productListing",
-        scroller: "body",
-        start: "35.3% 65px",
-        end: "35.3% -120%",
-        scrub: 1,
-        // markers: true,
-        pin: true,
-      },
-    });
-
-    tl.to(
-      "#productStrip1",
-      {
-        transform: `translateY(-56%)`,
-        duration: 1.4,
-      },
-      "a"
-    ).to(
-      "#productStrip2",
-      {
-        transform: `translateY(-61.5%)`,
-        duration: 1.4,
-      },
-      "a"
-    );
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 576) {
-        setDisplayedProducts(products.slice(0, 10));
-      } else {
-        setDisplayedProducts(products);
-      }
-    };
-
-    handleResize(); // check on mount
-
-    window.addEventListener("resize", handleResize); // update on resize
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
   return (
     <div className={styles.productListing} id="productListing">
       <div className={styles.leftProCon}>
@@ -110,7 +49,7 @@ const ProductListing = () => {
             width={1000}
             height={1000}
             alt="image"
-            src={displayedProducts[0]?.image1}
+            src={products[0]?.image1}
           />
         </Link>
 
@@ -149,7 +88,7 @@ const ProductListing = () => {
       </div>
       <div className={styles.rightProCon} id="productCont">
         <div className={styles.rightProConWrap}>
-          {displayedProducts?.slice(1, 11)?.map((product, index) => (
+          {products?.slice(1, 11)?.map((product, index) => (
             <div key={index} className={styles.productCard}>
               <Link href="/product?id=6752e99c935fd014e82be779">
                 <Image
@@ -193,41 +132,6 @@ const ProductListing = () => {
               </div>
             </div>
           ))}
-        </div>
-        <div className={styles.rightProConStrip} id="productStrip1">
-          {displayedProducts.slice(0, 7).map((product, index) => (
-            <Link
-              href={`/product?id=6752e99c935fd014e82be779`}
-              key={index}
-              className={styles.productCard}
-            >
-              <Image
-                width={1000}
-                height={1000}
-                alt="image"
-                src={product.image1}
-              />
-            </Link>
-          ))}
-        </div>
-        <div className={styles.rightProConStrip} id="productStrip2">
-          {displayedProducts
-            .reverse()
-            .slice(0, 8)
-            .map((product, index) => (
-              <Link
-                href={`/product?id=6752e99c935fd014e82be779`}
-                key={index}
-                className={styles.productCard}
-              >
-                <Image
-                  width={1000}
-                  height={1000}
-                  alt="image"
-                  src={product.image1}
-                />
-              </Link>
-            ))}
         </div>
       </div>
     </div>
