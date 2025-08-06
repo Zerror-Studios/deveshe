2d83675ee094c913b67d43188a85f1ba1000a415
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-08-06 17:32:51 +0530

    pdp hover and modal close

--- src/components/lookbookChapter/ProductContainer.jsx
+++ src/components/lookbookChapter/ProductContainer.jsx
@@ -14,7 +14,9 @@ const ProductContainer = ({ data }) => {
             className="product-lb"
             key={`product-${index}`}
           >
+            <div className="product_img_wrap">
             <Image width={1000} height={1000} src={path} alt={altText} />
+            </div>
             <div className="product-info">
               <p>{item?.name || ""}</p>
             </div>
--- src/components/product/ProductModalPreview.jsx
+++ src/components/product/ProductModalPreview.jsx
@@ -14,14 +14,15 @@ const ProductModalPreview = ({
   };
   if (!isOpen && Object.keys(data).length === 0) return;
   return (
-    <div className="ReactModalPortal_img_cntr" data-lenis-prevent>
+    <div className="ReactModalPortal_img_cntr" data-lenis-prevent onClick={handleReset}>
       <div className="ReactModalPortal_img_cntr_overlay">
         <div className="ReactModalPortal_img_cntr_cursor">
-          <div className="ReactModalPortal_img_cross" onClick={handleReset}>
+          <div className="ReactModalPortal_img_cross">
             <RxCross2 />
           </div>
           <div className="ReactModalPortal_img_cntr_grid">
             <div className="ReactModalPortal_img_cntr_grid_cover">
+              <div className="ReactModalPortal_img_loader"></div>
               <Image src={data?.path || ""} alt={data?.altText || ""} fill objectFit="contain"/>
             </div>
           </div>
