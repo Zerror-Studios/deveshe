17da0f7b89e1899bcdae365d9123985b93474bfe
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2026-05-20 15:01:34 +0530

    Updated filter popup

--- src/components/home/HeroSectionHome.jsx
+++ src/components/home/HeroSectionHome.jsx
@@ -5,16 +5,16 @@ const HeroSectionHome = () => {
     return (
         <div id="hero_section_home">
             <div className="home_banner_container_inner">
-                <Image src="/assets/images/home/Homepage 1.webp" alt="home_banner" width={1000} height={1000} />
+                <Image src="/assets/images/home/Homepage1.webp" alt="home_banner" width={1000} height={1000} />
             </div>
             <div className="home_banner_container_inner">
-                <Image src="/assets/images/home/Homepage 2.webp" alt="home_banner" width={1000} height={1000} />
+                <Image src="/assets/images/home/Homepage1.webp" alt="home_banner" width={1000} height={1000} />
             </div>
             <div className="home_banner_container_inner">
-                <Image src="/assets/images/home/Homepage 3.webp" alt="home_banner" width={1000} height={1000} />
+                <Image src="/assets/images/home/Homepage1.webp" alt="home_banner" width={1000} height={1000} />
             </div>
             <div className="home_banner_container_inner">
-                <Image src="/assets/images/home/Homepage 4.webp" alt="home_banner" width={1000} height={1000} />
+                <Image src="/assets/images/home/Homepage1.webp" alt="home_banner" width={1000} height={1000} />
             </div>
         </div>
     )
