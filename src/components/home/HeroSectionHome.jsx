8406c893e41ed760d6cecdc8054d6747b1f84bfe
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2026-05-20 15:02:13 +0530

    image path update

--- src/components/home/HeroSectionHome.jsx
+++ src/components/home/HeroSectionHome.jsx
@@ -8,13 +8,13 @@ const HeroSectionHome = () => {
                 <Image src="/assets/images/home/Homepage1.webp" alt="home_banner" width={1000} height={1000} />
             </div>
             <div className="home_banner_container_inner">
-                <Image src="/assets/images/home/Homepage1.webp" alt="home_banner" width={1000} height={1000} />
+                <Image src="/assets/images/home/Homepage2.webp" alt="home_banner" width={1000} height={1000} />
             </div>
             <div className="home_banner_container_inner">
-                <Image src="/assets/images/home/Homepage1.webp" alt="home_banner" width={1000} height={1000} />
+                <Image src="/assets/images/home/Homepage3.webp" alt="home_banner" width={1000} height={1000} />
             </div>
             <div className="home_banner_container_inner">
-                <Image src="/assets/images/home/Homepage1.webp" alt="home_banner" width={1000} height={1000} />
+                <Image src="/assets/images/home/Homepage4.webp" alt="home_banner" width={1000} height={1000} />
             </div>
         </div>
     )
